85df0d1a270047b32b0446fe12f6ba151457823d
inkollu akashdhar <akashdhar.apssdc@gmail.com>
2020-04-13 21:34:59 +0530

    first commit

---
+++ src/main/java/com/covid/oc/OcApplication.java
@@ -0,0 +1,13 @@
+package com.covid.oc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcApplication.class, args);
+	}
+
+}
